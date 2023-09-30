d041621498e4c31423379f1b2af21aad1a3c7f6c
mamlaki <melekredwan@icloud.com>
2023-09-30 01:26:08 -0600

    Created Calculator.cs with basic operations.

---
+++ SumItUp/Data/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SumItUp.Data
+{
+	public class Calculator
+	{
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public double Subtract(double a, double b)
+		{
+			return a - b;
+		}
+
+		public double Multiply(double a, double b)
+		{
+			return a * b;
+		}
+
+		public double Divide(double a, double b)
+		{
+			if (b != 0)
+			{
+				return a / b;
+			}
+			throw new DivideByZeroException("Cannot divide by zero.");
+		}
+	}
+}
+

a6b5709a8c95b014c460244dee068ee1edd30974
mamlaki <melekredwan@icloud.com>
2023-09-30 03:49:10 -0600

    Added matrix operations for addition, subtraction, and multiplication.

--- SumItUp/Data/Calculator.cs
+++ SumItUp/Data/Calculator.cs
@@ -64,6 +64,72 @@ namespace SumItUp.Data
 			}
 			throw new ArgumentException("Natural log requires a positive argument");
 		}
+
+		public class Matrix
+		{
+			public double[,] Value { get; set; }
+
+			public Matrix(double[,] value)
+			{
+				Value = value;
+			}
+
+			public Matrix Add(Matrix other)
+			{
+				var rows = Value.GetLength(0);
+				var cols = Value.GetLength(1);
+				var result = new double[rows, cols];
+				for (int i = 0; i < rows; i++)
+				{
+					for (int j = 0; j < cols; j++)
+					{
+						result[i, j] = Value[i, j] + other.Value[i, j];
+					}
+				}
+				return new Matrix(result);
+			}
+
+			public Matrix Subtract(Matrix other)
+			{
+				var rows = Value.GetLength(0);
+				var cols = Value.GetLength(1);
+				var result = new double[rows, cols];
+				for (int i = 0; i < rows; i++)
+				{
+					for (int j = 0; j < cols; j++)
+					{
+						result[i, j] = Value[i, j] - other.Value[i, j];
+					}
+				}
+				return new Matrix(result);
+			}
+
+			public Matrix Multiply(Matrix other)
+			{
+				var rowsA = Value.GetLength(0);
+				var rowsB = other.Value.GetLength(0);
+				var colsA = Value.GetLength(1);
+				var colsB = other.Value.GetLength(1);
+
+				if (colsA != rowsB)
+				{
+					throw new InvalidOperationException("Non-conformable matrices.");
+				}
+
+				var result = new double[rowsA, colsB];
+				for(int i = 0; i < rowsA; i++)
+				{
+					for (int j = 0; j < colsB; j++)
+					{
+						for (int k = 0; k < colsA; k++)
+						{
+							result[i, j] += Value[i, k] * other.Value[k, j];
+						}
+					}
+				}
+				return new Matrix(result);
+			}
+		}
 	}
 }
 
